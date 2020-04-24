b1460ab045f1ab9d94efea7e14744165b4d0862d
CherelThibaut <55751931+CherelThibaut@users.noreply.github.com>
2020-04-24 09:36:20 +0200

    Revert "Navbar beug enlever li fonctionne"
    This reverts commit 1c1ae0d2906a2d5dfe4b05326b6bc6afeb927d37.

--- front-react/src/screens/Navbar.js
+++ front-react/src/screens/Navbar.js
@@ -1,41 +1,38 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 import '../css/navbar.css';
 import { disconnect } from '../store/actions/userActions';
-import { connect } from 'react-redux';
+import {connect} from 'react-redux';
 
-function Navbar(props) {
-    return (
+function Navbar(props){
+    return(
         <nav>
             <ul>
                 <li>
                     <Link to="/">Home</Link>
                 </li>
-                {props.isLogin?
-                    (
-                        <li>
-                            <Link to="/calendar">Calendar</Link>
-                        </li>,
-                        <li>
-                            <Link to="/listRequest">Requests</Link>
-                        </li>, 
-                        <li className="logout" onClick={() => { props.disconnect(); window.location = "/" }}>Logout</li>
-                    ) : (
-                        <li>
-                            <Link to="/login">Login</Link>
-                        </li>,
-                        <li>
-                            <Link to="/register">Register</Link>
-                        </li>
-                    )
-                }
+                <li>
+                    <Link to="/calendar">Calendar</Link>
+                </li>
+                <li>
+                    <Link to="/listRequest">Requests</Link>
+                </li>
+                <div style={{float:"right"}}>
+                <li>
+                    <Link to="/login">Login</Link>
+                </li>
+                <li>
+                    <Link to="/register">Register</Link>
+                </li>
+                <li className="logout" onClick={()=>{props.disconnect(); window.location="/"}}>Logout</li>
+                </div>
             </ul>
         </nav>
     )
 }
 const mapDispatchToProps = dispatch => {
     return {
-        disconnect: () => { dispatch(disconnect()); }
+        disconnect:() => {dispatch(disconnect());}
     };
 };
 
