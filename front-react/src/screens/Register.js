226db454745542a75c770aca76482894821f2bf6
mlima95 <milan.lima@hotmail.fr>
2020-04-24 12:42:07 +0200

    add css register

--- front-react/src/screens/Register.js
+++ front-react/src/screens/Register.js
@@ -37,25 +37,33 @@ class Register extends Component {
     }
     render() {
         return (
-            <div className="container">
-                <div className="row">
-                    <form noValidate onSubmit={this.onSubmit}>
+            <div className="row" style={{ flexDirection: "column" }}>
+            <div className="cardDescription" >
+                    <form noValidate onSubmit={this.onSubmit} style={{
+                            display: "flex", flexDirection: "column",
+                            alignSelf: "center",marginLeft:"20px",marginRight:"20px"
+                        }}>
                         <label>Gender</label>
-                        <input type="radio" id="Gender" name="Gender" value="male" label="male" checked={this.state.Gender === 'male'} onChange={this.onChange} ></input>
-                        <input type="radio" id="Gender" name="Gender" value="female" label="female" checked={this.state.Gender === 'female'} onChange={this.onChange}></input>
+                        {/* <input type="radio" id="Gender" name="Gender" value="male" label="male" checked={this.state.Gender === 'male'} onChange={this.onChange} ></input>
+                        <input type="radio" id="Gender" name="Gender" value="female" label="female" checked={this.state.Gender === 'female'} onChange={this.onChange}></input> */}
+                        <select className="inputLogin" name="Gender" value={this.state.Gender ? this.state.Gender:"undefined"} onChange={this.onChange}>
+                    <option value="">--Please choose an option--</option>
+                        <option value="male">Homme</option>
+                        <option value="female">Femme</option>
+                    </select>
                         <label>Firstname</label>
-                        <input type="text" name="Firstname" placeholder="Enter firstname" value={this.state.Firstname} onChange={this.onChange}></input>
+                        <input className="inputLogin" type="text" name="Firstname" placeholder="Enter firstname" value={this.state.Firstname} onChange={this.onChange}></input>
                         <label>Lastname</label>
-                        <input type="text" name="Lastname" placeholder="Enter lastname" value={this.state.Lastname} onChange={this.onChange}></input>
+                        <input className="inputLogin" type="text" name="Lastname" placeholder="Enter lastname" value={this.state.Lastname} onChange={this.onChange}></input>
                         <label>Number</label>
-                        <input type="text" name="Number" placeholder="Enter number" value={this.state.Number} onChange={this.onChange}></input>
+                        <input className="inputLogin" type="text" name="Number" placeholder="Enter number" value={this.state.Number} onChange={this.onChange}></input>
                         <label>Birthday</label>
-                        <input type="Date" name="Birthday" value={this.state.Birthday} onChange={this.onChange}></input>
+                        <input className="inputLogin" type="Date" name="Birthday" value={this.state.Birthday} onChange={this.onChange}></input>
                         <label>Email</label>
-                        <input type="email" name="Email" placeholder="Enter email" value={this.state.email} onChange={this.onChange}></input>
+                        <input className="inputLogin" type="email" name="Email" placeholder="Enter email" value={this.state.email} onChange={this.onChange}></input>
                         <label>Password</label>
-                        <input type="password" name="Password" placeholder="Enter password" value={this.state.Password} onChange={this.onChange}></input>
-                        <button type="submit">Sign up</button>
+                        <input className="inputLogin" type="password" name="Password" placeholder="Enter password" value={this.state.Password} onChange={this.onChange}></input>
+                        <button className="btnLogin" type="submit">Sign up</button>
                     </form>
                 </div>
             </div>
