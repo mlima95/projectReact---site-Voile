99c7d79896cfad290a1275289faad083ea445cfa
CherelThibaut <55751931+CherelThibaut@users.noreply.github.com>
2020-04-24 15:10:00 +0200

    update home

--- front-react/src/screens/Home.js
+++ front-react/src/screens/Home.js
@@ -9,23 +9,18 @@ var sectionStyle = {
   };
   var section2Style = {
     width: "75%",
-    left: "10%",
+    left: "5%",
     top: "20%",
 	display: "block" ,
 	position: "absolute",
     background: "rgba(255,255,255,0.4)",
-    textAlign:"center"
+    textAlign:"justify",
+    paddingLeft:"10%",
+    paddingTop:"3%",
+    paddingBottom:"3%",
+    paddingRight:"5%"
   };
-  var section3Style = {
-    width: "100%",
 
-    
-  };
-  var section4Style = {
-    width: "100%",
-
-    
-  };
 export default class Home extends React.Component{
 
 
@@ -33,14 +28,14 @@ export default class Home extends React.Component{
         return(
              <section style={ sectionStyle }>
                  <section style={ section2Style }>
-                    <h2>Votre Centre Nautique</h2>
+                    <img src="https://i.ibb.co/tBRyqVf/kisspng-sailing-ship-boating-logo-5c023f185a0969-8793135915436510963688.png" height="150px"  width="150px" />
+                    <h2>Votre Centre Nautique</h2> 
+                    <br /><br />              
                     Votre Centre Nautique est ravi de vous accueillir sur son site de réservation. Fondé en 1976, le VCN est une association de voile créée par des professionnels souhaitant faire partager leur passion.
-                 </section>
-                 <section style={ section3Style }>
-                 
-                 </section>
-                 <section style={ section4Style }>
-                 
+                    <br /><br />
+                    Nous vous proposons sur ce site la possibilité d'organiser des séances de locations de matériel (Voile, Catamaran, Paddle). Pour cela, vous devez créer un compte et vous connecter.
+                    <br /><br />
+                    Vous aurez alors également accès au calendrier, répertoriant les différents rendez-vous vous concernant, ainsi que l'état de validation de ceux-ci.
                  </section>
              </section>
         );
